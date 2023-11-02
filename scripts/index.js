672231dbff286df285b1554a085b03f29566462d
Owen Luts <oluts47@gmail.com>
2023-11-02 11:35:07 -0400

    shit

--- scripts/index.js
+++ scripts/index.js
@@ -1,14 +1,18 @@
 const imgCarousel = document.getElementById('img-carousel');
 const buttons = document.getElementsByClassName('button');
 
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].id = i * -100;
-    buttons[i].addEventListener("click", () => {
-        for (let button of buttons) {
-            button.classList.remove('selected');
-        }
-        buttons[i].classList.add('selected');
-        currentSlide = buttons[i].getAttribute('value');
-        imgCarousel.style.transform = `translate(${buttons[i].id}vw)`
-    });
+function buttonClicks() {
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].id = i * -100;
+        buttons[i].addEventListener("click", () => {
+            for (let button of buttons) {
+                button.classList.remove('selected');
+            }
+            buttons[i].classList.add('selected');
+            currentSlide = buttons[i].getAttribute('value');
+            imgCarousel.style.transform = `translate(${buttons[i].id}vw)`
+        });
+    };
 }
+
+buttonClicks();
--- scripts/projects.js
+++ scripts/projects.js
@@ -2,37 +2,73 @@ const project1 = {
     name: "DueIt",
     month: "September",
     year: "2023",
-    description: "give your balls a tug",
+    description: "Organization through interaction",
+    imgPath: "../icons/dueit.png",
+    link: "https://devpost.com/software/just-another-file-organizer#updates",
 };
 const project2 = {
     name: "PublicEye",
     month: "September",
     year: "2023",
     description: "i see you",
+    imgPath: "../icons/public-eye.png",
+    link: "https://devpost.com/software/public-eye",
 };
+const project3 = {
+    name: "Jafo",
+    month: "October",
+    year: "2023",
+    description: "Just another file organizer",
+    imgPath: "../icons/jafo.png",
+    link: "https://github.com/Ascensi0nn/jafo",
+}
 
-
-const projects = [project1, project2]
+const projects = [project1, project2, project3]
 const projectHolder = document.getElementById('project-holder')
 
+function createHorizontalBar() {
+    const bar = document.createElement('div');
+    bar.classList.add('bar');
+    projectHolder.appendChild(bar);
+}
+
 for (let i = 0; i < projects.length; i++) {
+    createHorizontalBar();
+
     const p = document.createElement('div');
-    const title = document.createElement('h3');
+    p.style.display = 'flex';
+    p.style.justifyContent = 'space-evenly';
+    if (i % 2 == 0) { p.style.flexDirection = "row"; }
+    else { p.style.flexDirection = "row-reverse"; }
+
+    const img = document.createElement('img');
+    img.src = projects[i]['imgPath'];
+    img.style.height = '35vh';
+    img.style.borderRadius = '2rem';
+
+    const sideBar = document.createElement('div');
+    sideBar.style.display = 'flex';
+    sideBar.style.flexDirection = 'column';
+    sideBar.style.alignItems = 'center';
+
+    const title = document.createElement('h2');
+    title.style.marginBottom = '0px';
     title.classList.add('light')
     title.innerText = projects[i]['name'];
-    const img = document.createElement('img');
-    const date = document.createElement('p');
-    const description = document.createElement('p');
+
+    const date = document.createElement('h5');
+    date.innerText = projects[i]['month'] + ", " + projects[i]['year'];
+    date.classList.add('light');
     
-    if (i % 2 == 0) {
+    const description = document.createElement('p');
+    description.innerText = projects[i]['description'];
+    description.classList.add('light');
 
-    }
-    else {
+    sideBar.appendChild(title);
+    sideBar.appendChild(date);
+    sideBar.appendChild(description);
 
-    }
-    p.appendChild(title);
     p.appendChild(img);
-    p.appendChild(date);
-    p.appendChild(description);
+    p.appendChild(sideBar);
     projectHolder.appendChild(p);
 }
